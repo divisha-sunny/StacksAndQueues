6309cc686cb9832e3ad4e94f570d6dc977f32623
C. Divisha <chowdavaramdivisha@gmail.com>
2020-03-13 18:01:18 +0530

    "Sorting stack using Recursion" code changed

--- src/StackProblems/SortStackUsingRecursion.java
+++ src/StackProblems/SortStackUsingRecursion.java
@@ -5,22 +5,38 @@ import java.util.Stack;
 
 public class SortStackUsingRecursion {
 
-    public void sortStack(Stack<Integer> s) {
-        if (!s.isEmpty()) {
-            int temp = s.pop();
-            sortStack(s);
-            sortedInsert(s, temp);
-        }
-    }
-     public void sortedInsert(Stack<Integer> s, int temp){
-        if(s.isEmpty() || temp>s.peek()){
-            s.push(temp);
+    static void sortedInsert(Stack<Integer> s, int x)
+    {
+        // Base case: Either stack is empty or newly inserted
+        // item is greater than top (more than all existing)
+        if (s.isEmpty() || x > s.peek())
+        {
+            s.push(x);
             return;
         }
-        else{
-         int tempS = s.pop();
-         sortedInsert(s, tempS);
-         s.push(tempS);
+
+        // If top is greater, remove the top item and recur
+        int temp = s.pop();
+        sortedInsert(s, x);
+
+        // Put back the top item removed earlier
+        s.push(temp);
+    }
+
+    // Method to sort stack
+    public void sortStack(Stack<Integer> s)
+    {
+        // If stack is not empty
+        if (!s.isEmpty())
+        {
+            // Remove the top item
+            int x = s.pop();
+
+            // Sort remaining stack
+            sortStack(s);
+
+            // Push the top item back in sorted stack
+            sortedInsert(s, x);
         }
     }
     public void printStack(Stack<Integer> stack){
